8752e9bbd50a9d3c1a823991e4fed1a17ec2e0fa
Ben Barantschik <bbarantschik@gmail.com>
2018-08-18 04:26:32 -0700

    Buggy implementation of new architecture

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import shutil
 import json
 import struct
 import time
+from utils import ContextLoader, ComboVocab
 
 argument_parser = argparse.ArgumentParser(description = "Train a variable name predictor for Java")
 
@@ -20,20 +21,14 @@ argument_parser.add_argument("-e", "--epochs", default = 1, type = int,
 argument_parser.add_argument("-b", "--batch-size", default = 32, type = int,
                 help = "Number of frame groups to process at a time")
 
-argument_parser.add_argument("-nh", "--num-hidden", default = 256, type = int,
-                help = "Number of hidden weights per GRU layer")
+argument_parser.add_argument("-nh", "--num-hidden-per-side", default = 256, type = int,
+                help = "Number of hidden weights per GRU layer in the forward/backward networks")
 
 argument_parser.add_argument("-nl", "--num-layers", default = 2, type = int,
                 help = "Number of GRU layers")
 
-argument_parser.add_argument("--token-embed-size", default = 240, type = int,
-                help = "Size of token embeddings")
-
-argument_parser.add_argument("--type-embed-size", default = 16, type = int,
-                help = "Size of type embeddings")
-
-argument_parser.add_argument("-t", "--time", default = 256, type = int,
-                help = "How many tokens to backpropagate error to in the past")
+argument_parser.add_argument("--embedding-size", default = 256, type = int,
+                help = "Size of embeddings")
 
 argument_parser.add_argument("--clip", default = 0.2, type = float,
                 help = "Amount to clip weights by.")
@@ -44,11 +39,14 @@ argument_parser.add_argument("-lr", "--learning-rate", default = 1e-3, type = fl
 argument_parser.add_argument("--lr-decay", default = 1.0, type = float,
                 help = "Factor to multiply learning rate by every epoch.")
 
-argument_parser.add_argument("--time-limit", default = 1000000, type = int,
+argument_parser.add_argument("--time-limit", default = int(1e12), type = int,
                 help = "Maximum number of seconds this can train.")
 
 
 options = argument_parser.parse_args()
+
+with open('data_options.json') as data_options_f:
+    data_options = json.load(data_options_f)
 
 save_dir = options.save_dir_prefix + datetime.datetime.now().strftime('.%Y.%m.%d.%H.%M.%S/')
 if os.path.exists(save_dir):
@@ -56,147 +54,94 @@ if os.path.exists(save_dir):
     quit()
 
 os.makedirs(save_dir)
-shutil.copyfile(__file__, save_dir + 'java_mxnet.py')
-with open(save_dir + 'options.json', 'w') as options_file:
+shutil.copyfile(__file__, save_dir + 'train.py')
+with open(save_dir + 'run_options.json', 'w') as options_file:
     json.dump(vars(options), options_file, indent = 1)
 
 ctx = mx.gpu(3)
-batch_size = options.batch_size
-time_slice = options.time
-clip = options.clip
+vocab = ComboVocab()
 
-with open('java_token_vocab.pickle', 'rb') as f:
-    token_vocab = pickle.load(f)
-with open('java_type_vocab.pickle', 'rb') as f:
-    type_vocab = pickle.load(f)
-n_tokens = len(token_vocab.token_to_idx)
-n_types = len(type_vocab.token_to_idx)
-
-print("Number of tokens: %d, number of types: %d" % (n_tokens, n_types))
-
-class VocabModel(mx.gluon.Block):
-    def __init__(self, num_hidden, num_layers, token_embed_size, type_embed_size,
+class BidirectionalModel(mx.gluon.Block):
+    def __init__(self, vocab, hidden_per_side, num_layers, embedding_size,
                  dropout = 0.5, tie_weights = False, **kwargs):
-        super(VocabModel, self).__init__(**kwargs)
-        self.num_hidden = num_hidden
+        super(BidirectionalModel, self).__init__(**kwargs)
+        self.hidden_per_side = hidden_per_side
+        self.hidden_full = 2 * hidden_per_side
         self.drop = mx.gluon.nn.Dropout(dropout)
-        self.tok_encoder = mx.gluon.nn.Embedding(n_tokens, token_embed_size, weight_initializer = mx.init.Uniform(0.1))
-        self.type_encoder = mx.gluon.nn.Embedding(n_types, type_embed_size, weight_initializer = mx.init.Uniform(0.1))
-        self.rnn = mx.gluon.rnn.GRU(num_hidden, num_layers, dropout = dropout, input_size = token_embed_size + type_embed_size)
+        self.embedder = mx.gluon.nn.Embedding(vocab.total_tokens, embedding_size, weight_initializer = mx.init.Uniform(0.1))
+        self.forward_rnn = mx.gluon.rnn.GRU(hidden_per_side, num_layers, dropout = dropout, input_size = embedding_size)
+        self.backward_rnn = mx.gluon.rnn.GRU(hidden_per_side, num_layers, dropout = dropout, input_size = embedding_size)
+        self.output_rnn = mx.gluon.rnn.GRU(self.hidden_full, num_layers, dropout = dropout, input_size = embedding_size)
         if tie_weights:
-            self.decoder = mx.gluon.nn.Dense(n_tokens + n_types, in_units = num_hidden, params = self.encoder.params)
+            contract_hiddens = mx.gluon.nn.Dense(hidden_per_side, in_units = self.hidden_full)
+            decoder = mx.gluon.nn.Dense(vocab.total_tokens, in_units = hidden_per_side, params = self.encoder.params)
+            self.decoder = lambda x: decoder(contract_hiddens(x))
         else:
-            self.decoder = mx.gluon.nn.Dense(n_tokens + n_types, in_units = num_hidden)
+            decoder = mx.gluon.nn.Dense(vocab.total_tokens, in_units = self.hidden_full)
     
-    def forward(self, token_inputs, type_inputs, hidden):
-        token_embed = self.drop(self.tok_encoder(token_inputs))
-        type_embed = self.drop(self.type_encoder(type_inputs))
-        total_embed = mx.nd.concat(token_embed, type_embed, dim = 2)
-        output, hidden = self.rnn(total_embed, hidden)
-        output = self.drop(output)
-        total_decoded = self.decoder(output.reshape((-1, self.num_hidden))) 
-        tokens_decoded = total_decoded.slice_axis(axis = 1, begin = 0, end = n_tokens)
-        types_decoded = total_decoded.slice_axis(axis = 1, begin = n_tokens, end = None)
-        return tokens_decoded, types_decoded, hidden
-    
-    def begin_state(self, *args, **kwargs):
-        return self.rnn.begin_state(*args, **kwargs)
+    def forward(self, forward_contexts, backward_contexts, predict_in, is_training = True):
+        dropout_mode = 'training' if is_training else 'always'
+        batch_size = predict_in.shape[1]
+        n_contexts = forward_contexts.shape[0]
+        
+        # TODO(Ben) also add in option for variance?
+        avg_f_hidden, avg_b_hidden = None, None
+        for ci in range(n_contexts):
+            f_embed = self.drop(self.embedder(forward_contexts[ci]), mode = dropout_mode)
+            b_embed = self.drop(self.embedder(backward_contexts[ci]), mode = dropout_mode)
+        
+            f_hidden = self.forward_rnn.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
+            _, f_hidden = self.forward_rnn(f_embed, f_hidden)
+            b_hidden = self.nackward_rnn.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
+            _, b_hidden = self.forward_rnn(f_embed, b_hidden)
+            
+            if avg_f_hidden is None:
+                avg_f_hidden = f_hidden
+                avg_b_hidden = b_hidden
+            else:
+                for hid_i in range(len(f_hidden)):
+                    avg_f_hidden[hid_i] += f_hidden[hid_i]
+                    avg_b_hidden[hid_i] += b_hidden[hid_i]
+        combined_hidden = [mx.nd.concatenate((f_h, b_h)) / n_contexts for f_h, b_h in zip(avg_f_hidden, avg_b_hidden)]
+        
+        predict_embed = self.drop(self.embedder(predict_in), mode = dropout_mode)
+        output, _ = self.output_rnn(predict_embed, combined_hidden)
+        output = self.drop(output, mode = dropout_mode)
+        output_decoded = self.decoder(output.reshape((-1, 2 * self.num_hidden)))
+        return output_decoded
 
-
-# x:x+time -> x+1:x+1+time
-def batches_gen(f_prefix, time_slice = 256, batch_size = 32, ctx = mx.gpu(0)):
-    data = open(f_prefix + '.bin', 'rb')
-    with open(f_prefix + "_starts.pickle", 'rb') as f:
-        starts = pickle.load(f)
-    batch_starts = []
-    for i in range(len(starts) - 1):
-        batch_starts.extend(list(range(starts[i], starts[i + 1] - time_slice - 1)))
-    np.random.shuffle(batch_starts)
-    
-    batch_i = 0
-    while batch_i < len(batch_starts):
-        tokens_in, tokens_target, types_in, types_target = [np.zeros((batch_size, time_slice)) for i in range(4)]
-        for i in range(batch_size):
-            data_i = batch_starts[batch_i]
-            data.seek(data_i * 2)
-            slice_A = struct.unpack('ll', data.read(2))
-            slice_B = struct.unpack('ll', data.read(2))
-            for slice_i in range(time_slice):
-                tokens_in[i, slice_i] = slice_A[0]
-                types_in[i, slice_i] = slice_A[1]
-                tokens_target[i, slice_i] = slice_B[0]
-                types_target[i, slice_i] = slice_B[1]
-                if slice_i != time_slice - 1:
-                    slice_A = slice_B
-                    slice_B = struct.unpack('ll', data.read(2))
-            batch_i += 1
-        tokens_in = mx.nd.array(tokens_in, ctx = ctx).T
-        types_in = mx.nd.array(types_in, ctx = ctx).T
-        tokens_target = mx.nd.array(tokens_target, ctx = ctx).T.reshape((-1,))
-        types_target = mx.nd.array(types_target, ctx = ctx).T.reshape((-1,))
-        yield tokens_in, types_in, tokens_target, types_target
-    data.close()
-    
-def detach(hidden):
-    if isinstance(hidden, (tuple, list)):
-        hidden = [i.detach() for i in hidden]
-    else:
-        hidden = hidden.detach()
-    return hidden
-
-model = VocabModel(num_hidden = options.num_hidden,
-                   num_layers = options.num_layers,
-                   token_embed_size = options.token_embed_size,
-                   type_embed_size = options.type_embed_size)
+model = BidirectionalModel(vocab,
+                           hidden_per_side = options.hidden_per_side,
+                           num_layers = options.num_layers,
+                           embedding_sizee = options.embedding_size,
+                           dropout = options.dropout,
+                           tie_weights = options.tie_weights)
 model.hybridize()
 model.collect_params().initialize(mx.init.Xavier(), ctx = ctx)
 trainer = mx.gluon.Trainer(model.collect_params(), 'adam', {'learning_rate': options.learning_rate})
 loss = mx.gluon.loss.SoftmaxCrossEntropyLoss()
 
-def create_prediction(num = 16, tokens = ['public', 'class', 'HTTP', '{', 'public', 'static', 'void'],
-                      types = ["<'public'>", "<'class'>", "<Identifier>", "<'{'>}", "<'public'>", "<'static'>", "<'void'>"]):
-    tokens_in, types_in = mx.nd.zeros((1, batch_size), ctx = ctx), mx.nd.zeros((1, batch_size), ctx = ctx)
-    hidden = model.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
-    for i, (token, type_str) in enumerate(zip(tokens, types)):
-        tokens_in[0, 0] = token_vocab.to_indices(token)
-        types_in[0, 0] = type_vocab.to_indices(type_str)
-        tokens_out, types_out, hidden = model(tokens_in, types_in, hidden)
-    print(' '.join(tokens), end = ' ')
-    
-    for i in range(num):
-        best_token = np.argmax(tokens_out.reshape((1, batch_size, n_tokens))[0, 0, :].asnumpy())
-        best_type = np.argmax(types_out.reshape((1, batch_size, n_types))[0, 0, :].asnumpy())
-        print(token_vocab.to_tokens(int(best_token)), end = ' ')
-        tokens_out, types_out, hidden = model(tokens_in, types_in, hidden)
-        tokens_in[0, 0] = best_token
-        types_in[0, 0] = best_type
-    print()
-
-def most_likely(num = 16, tokens = ['public', 'class', 'HTTP', '{', 'public', 'static', 'void'],
-                types = ["<'public'>", "<'class'>", "<Identifier>", "<'{'>}", "<'public'>", "<'static'>", "<'void'>"]):
-    tokens_in, types_in = mx.nd.zeros((1, batch_size), ctx = ctx), mx.nd.zeros((1, batch_size), ctx = ctx)
-    hidden = model.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
-    for i, (token, type_str) in enumerate(zip(tokens, types)):
-        tokens_in[0, 0] = token_vocab.to_indices(token)
-        types_in[0, 0] = type_vocab.to_indices(type_str)
-        tokens_out, types_out, hidden = model(tokens_in, types_in, hidden)
-    tokens_out = tokens_out.reshape((1, batch_size, n_tokens))[0, 0, :].asnumpy()
-    top = np.argsort(tokens_out)[-num:][::-1]
-    print("Most likely:")
-    for i in range(num):
-        print(token_vocab.to_tokens(int(top[i])), '(%f)' % tokens_out[top[i]])
-        
-test_batches = batches_gen('mx_datasets/test', time_slice = time_slice, batch_size = batch_size, ctx = ctx)
-
-def validation_loss(num_minibatches = 1):
+validation_data = ContextLoader(vocab, batch_size = options.batch_size, folder_name = 'data/val_small')
+def validation_loss(num_minibatches = 10, verbose = False):
     global test_batches
     i = 0
     avg_loss = 0.0
-    for tokens_in, types_in, tokens_target, types_target in test_batches:
+    for pre_contexts, post_contexts, in_tokens, target_tokens in validation_data:
         i += 1
-        hidden = model.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
-        tokens_output, types_output, hidden = model(tokens_in, types_in, hidden)
-        L = loss(tokens_output, tokens_target) + loss(types_output, types_target)
+        output_tokens = model(pre_contexts, post_contexts, in_tokens)
+        
+        if verbose: # will only print out the first batches
+            for context_i in range(pre_contexts.shape[0]):
+                ids_before = [int(x) for x in pre_contexts[0, :, context_i].asnumpy()]
+                ids_after = [int(x) for x in post_contexts[0, :, context_i].asnumpy()]
+                print('context before:', vocab.to_tokens(ids_before))
+                print('context after:', vocab.to_tokens(ids_after))
+            
+            out_argmax = output_tokens.reshape((-1, options.batch_size, vocab.total_tokens))[:, 0, :].argmax(axis=1).asnumpy()
+            print("The most likely token stream is:", vocab.to_tokens([int(x) for x in out_argmax]))
+            
+        
+        L = loss(output_tokens, target_tokens) # TODO(Ben): zero out the irrelevant padding tokens
         avg_loss += mx.nd.mean(L).asscalar()
         if i == num_minibatches:
             break
@@ -205,6 +150,7 @@ def validation_loss(num_minibatches = 1):
         return validation_loss(num_minibatches)
     return avg_loss / num_minibatches
 
+
 validation_losses = []
 start_global = time.time()
 for epoch in range(options.epochs):
@@ -212,24 +158,23 @@ for epoch in range(options.epochs):
     cur_lr = options.learning_rate * options.lr_decay ** epoch
     print("\nCurrent learning rate: %f" % cur_lr)
     trainer.set_learning_rate(cur_lr)
-    train_batches = batches_gen('mx_datasets/train', time_slice = time_slice, batch_size = batch_size, ctx = ctx)
-    for tokens_in, types_in, tokens_target, types_target in train_batches:
-        hidden = model.begin_state(func = mx.nd.zeros, batch_size = batch_size, ctx = ctx)
-        # hidden = detach(hidden)
+    train_data = ContextLoader(vocab, batch_size = 32, folder_name = 'data/train_small', ctx = ctx)
+    for pre_contexts, post_contexts, in_tokens, target_tokens in train_data:
         with mx.autograd.record():
-            tokens_output, types_output, hidden = model(tokens_in, types_in, hidden)
-            L = loss(tokens_output, tokens_target) + loss(types_output, types_target)
+            output_tokens = model(pre_contexts, post_contexts, in_tokens)
+            L = loss(output_tokens, target_tokens) # TODO(Ben): zero out the irrelevant padding tokens
             L.backward()
         grads = [i.grad(ctx) for i in model.collect_params().values()]
-        mx.gluon.utils.clip_global_norm(grads, clip * time_slice * batch_size)
+        mx.gluon.utils.clip_global_norm(grads, options.clip * data_options['context_width'] * options.batch_size) # TODO(Ben): adjust clipping for type of network
         trainer.step(batch_size)
-        validation_losses.append(validation_loss())
+        
+        if i % 100 == 0:
+            validation_losses.append(validation_loss())
         if i % 500 == 0:
             print("i = %d. Saving." % i)
-            model.save_params(save_dir + 'epoch-%d-i-%.6d.params' % (epoch, i))
+            model.save_params(save_dir + 'epoch-%d-i-%.6d.params' % (epoch, len(validation_losses)))
+            validation_loss(num_minibatches = 1, verbose = True)
             np.save(save_dir + 'validation_losses', np.array(validation_losses))
-            create_prediction()
-            most_likely()
         if time.time() - start_global > options.time_limit:
             print("Time limit reached. Ending epoch.")
             break
